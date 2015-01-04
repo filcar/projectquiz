41aed596c061f581df5d2bba6bf5b20c77cc3f7d
geodom61 <geodom61@geodom61-PC>
2015-01-04 13:38:36 +0200

    commit No1

--- projectQuiz/admin/question_delete.php
+++ projectQuiz/admin/question_delete.php
@@ -1,4 +1,16 @@
 <?php
 
+require_once("../includes/connection.php");
+include_once("../includes/functions.php");
+session_start();
+check_role(1);
+	
+$id=$_GET["id"];
+	
+mysql_query("DELETE FROM questions WHERE ID='".$id."'");
+
+header("Location: categories.php");
+	
+
 ?>
 
